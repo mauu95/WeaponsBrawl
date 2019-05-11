d8cd47be0e66d975506e852b9e37530fd878c0b4
mauu95 <mauu95@hotmail.it>
2019-05-11 13:39:54 +0200

    Introducted HP, damage who is inside explosion

--- WeaponsBrawlUnityProject/Assets/Scripts/BulletScript.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/BulletScript.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-public class BulletScript : NetworkBehaviour {
+public class BulletScript : NetworkBehaviour
+{
+
+    [Range(2, 60)] [SerializeField] public int ExplosionRadius = 2;
 
     public float speed = 20f;
     public Rigidbody2D rb;
-    [Range(2, 60)] [SerializeField] public int ExplosionRadius = 2;
     public MapController map;
-
     public GameObject explosionEffect;
+    public int BulletPower = 20;
 
-    void Start () {
+    void Start()
+    {
         rb.velocity = transform.right * speed;
         map = FindObjectOfType<MapController>();
     }
@@ -32,14 +35,32 @@ public class BulletScript : NetworkBehaviour {
             if (x * x + y * y - ExplosionRadius * ExplosionRadius < 0)
             {
                 Vector3Int position = Vector3Int.FloorToInt(gameObject.transform.position);
-                position.z = 0; // A volte diventa -1 a caso
+                position.z = 0; // A volte diventa -1 a caso quindi lo forzo a 0 io
                 Vector3Int destroyPos = position + p;
-                map.CmdDestroyTile( destroyPos.x, destroyPos.y, destroyPos.z);
+                map.CmdDestroyTile(destroyPos.x, destroyPos.y, destroyPos.z);
             }
         }
         explosionEffect.transform.localScale = new Vector3(ExplosionRadius, ExplosionRadius, 0);
         Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        DamageWhoIsInsideTheExplosion();
     }
 
-    
+    void DamageWhoIsInsideTheExplosion()
+    {
+        Collider2D[] hittedList = Physics2D.OverlapCircleAll(transform.position, ExplosionRadius);
+        foreach (Collider2D hitted in hittedList)
+        {
+            if (hitted.CompareTag("Player"))
+            {
+                hitted.gameObject.GetComponent<PlayerHealth>().TakeDamage(Mathf.FloorToInt(BulletPower / 2)); // divido per 2 perchè il player ha 2 collider e TakeDamage viene chiamato per ogni collider
+            }
+        }
+    }
+
+    //Gizmo explosion radius
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, ExplosionRadius);
+    }
 }
---
+++ WeaponsBrawlUnityProject/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHealth : NetworkBehaviour {
+    public int hp = 100;
+	
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            CmdRpcPlayerDie();
+        }
+    }
+
+    [Command]
+    [ClientRpc]
+    void CmdRpcPlayerDie()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
