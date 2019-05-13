6f6e6d480ccf537d8475ea367ecc387d32d31041
mauu95 <mauu95@hotmail.it>
2019-05-13 17:19:33 +0200

    OldSpawnPlayerScriptRestored

--- WeaponsBrawlUnityProject/Assets/Scripts/SpawnPlayer.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/SpawnPlayer.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.SceneManagement;
 
 
-public class SpawnPlayer : NetworkBehaviour {
+public class SpawnPlayer : NetworkBehaviour
+{
 
     private GameObject player;
     public GameObject playerToSpawn;
     public GameObject mainCamera;
-    public GameObject  virtualCamera;
-	
-	void Start () {
+    public GameObject virtualCamera;
+
+    void Start()
+    {
         if (isLocalPlayer)
         {
-            SetServerPlayerParent();
             CmdSpawnPlayer();
             mainCamera.SetActive(true);
             virtualCamera.SetActive(true);
@@ -25,15 +25,16 @@ public class SpawnPlayer : NetworkBehaviour {
             mainCamera.SetActive(false);
             virtualCamera.SetActive(false);
         }
-	}
+
+    }
 
     [Command]
     public void CmdSpawnPlayer()
     {
         player = (GameObject)Instantiate(playerToSpawn, transform);
         NetworkServer.SpawnWithClientAuthority(player, connectionToClient);
-        RpcSetParent(player, this.gameObject);
         RpcSetCameraFollow(player);
+
     }
     [ClientRpc]
     public void RpcSetCameraFollow(GameObject p)
@@ -43,37 +44,5 @@ public class SpawnPlayer : NetworkBehaviour {
             Cinemachine.CinemachineVirtualCamera virtualController = virtualCamera.GetComponent<Cinemachine.CinemachineVirtualCamera>();
             virtualController.m_Follow = p.transform;
         }
-    }
-
-    [ClientRpc]
-    public void RpcSetParent(GameObject child, GameObject parent)
-    {
-        child.transform.SetParent(parent.transform);
-    }
-
-    public void SetServerPlayerParent()
-    {
-        var scene = SceneManager.GetActiveScene();
-        GameObject[] RootGameObjs = scene.GetRootGameObjects();
-        GameObject player = FindInArrayByName(RootGameObjs, "Player(Clone)");
-        if (player)
-        {
-            GameObject playerObject = FindInArrayByName(RootGameObjs, "PlayerObject(Clone)");
-            player.transform.SetParent(playerObject.transform);
-        }
-    }
-
-    GameObject FindInArrayByName(GameObject[] array, string name)
-    {
-        GameObject result = null;
-        foreach(GameObject gmobj in array)
-        {
-            if (gmobj.name == name)
-            {
-                result = gmobj;
-                break;
-            }
-        }
-        return result;
     }
 }
