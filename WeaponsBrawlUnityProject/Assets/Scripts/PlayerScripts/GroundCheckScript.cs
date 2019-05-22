53239bd2fc0a7ac6879a427a6983ad88e0820a74
draolo <34096335+draolo@users.noreply.github.com>
2019-05-23 01:10:47 +0200

    Refactoring: removed debug message

--- WeaponsBrawlUnityProject/Assets/Scripts/PlayerScripts/GroundCheckScript.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/PlayerScripts/GroundCheckScript.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCheckScript : MonoBehaviour {
-    private List<string> unjampableColliderTag;
+    private List<string> notGroundedOnTag;
     public string[] ignoreTag = { "Chest", "Destroyer" };
     private PlayerMovement movementScript;
 
     private void Start()
     {
-        unjampableColliderTag = new List<string>(ignoreTag);
+        notGroundedOnTag = new List<string>(ignoreTag);
         movementScript = GetComponentInParent<PlayerMovement>();
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log(collision.tag);
-        if (!unjampableColliderTag.Contains(collision.tag))
+        if (!notGroundedOnTag.Contains(collision.tag))
         {
             movementScript.isGrounded = true;
         }
