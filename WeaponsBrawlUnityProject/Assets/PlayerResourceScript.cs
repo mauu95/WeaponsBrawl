b823eaae241afb2b5d54b23094d22baa54b080f1
mauu95 <mauu95@hotmail.it>
2019-05-22 12:52:02 +0200

    Player can collect resources

---
+++ WeaponsBrawlUnityProject/Assets/PlayerResourceScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResourceScript : MonoBehaviour {
+    public int resources;
+
+    public void addResouces(int amount)
+    {
+        resources += amount;
+    }
+}
--- WeaponsBrawlUnityProject/Assets/Scripts/PlayerScripts/PlayerAxeScript.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/PlayerScripts/PlayerAxeScript.cs
@@ -5,11 +5,22 @@ using UnityEngine.Networking;
 
 public class PlayerAxeScript : NetworkBehaviour
 {
+    private PlayerResourceScript resource;
+    public GameObject player;
+
+    private void Start()
+    {
+        resource = player.GetComponent<PlayerResourceScript>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Tree"))
+        {
+            resource.addResouces(50);
             Destroy(collision.gameObject);
+        }
+            
     }
 
 }
