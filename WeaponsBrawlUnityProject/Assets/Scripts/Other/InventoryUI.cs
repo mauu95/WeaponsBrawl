adb21b981512996ffe10573a0a253a8e4ac69e27
mauu95 <mauu95@hotmail.it>
2019-05-25 18:34:53 +0200

    Dynamic Inventory slots growth

--- WeaponsBrawlUnityProject/Assets/Scripts/Other/InventoryUI.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/Other/InventoryUI.cs
@@ -8,9 +8,11 @@ public class InventoryUI : MonoBehaviour {
     public Transform itemsParent;
     public GameObject inventoryUI;
     public GameObject Player;
+    public GameObject SlotPrefab;
 
     private PlayerWeaponManager_Inventory inventory;
     private InventorySlot[] slots;
+    private int Size = 1; // 2 Initial weapon 
 
     public void InitializeInventoryUI(GameObject player)
     {
@@ -33,8 +35,20 @@ public class InventoryUI : MonoBehaviour {
         int i = 0;
         foreach(AbstractWeaponGeneric weapon in inventory.Weapons)
         {
+            if (i > Size)
+            {
+                addSlot();
+                Size++;
+            }
             slots[i].AddItem(weapon);
             i++;
         }
+    }
+
+    public void addSlot()
+    {
+        GameObject newSlot = Instantiate(SlotPrefab);
+        newSlot.transform.SetParent(itemsParent);
+        newSlot.transform.localScale = Vector3.one;
     }
 }
