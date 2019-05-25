959886a201b2f2f4f636f1a6e6989557a228d5f3
mauu95 <mauu95@hotmail.it>
2019-05-25 16:28:47 +0200

    Added Inventory UI
    Riordinato scripts

---
+++ WeaponsBrawlUnityProject/Assets/Scripts/Other/InventorySlot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+
+    AbstractWeaponGeneric item;
+
+    public void AddItem(AbstractWeaponGeneric newItem)
+    {
+        item = newItem;
+        icon.sprite = item.info.icon;
+        icon.enabled = true;
+    }
+
+
+
+}
---
+++ WeaponsBrawlUnityProject/Assets/Scripts/Other/InventoryUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+    public GameObject Player;
+
+    private PlayerWeaponManager_Inventory inventory;
+    private InventorySlot[] slots;
+
+    public void InitializeInventoryUI(GameObject player)
+    {
+        Player = player;
+        inventory = Player.GetComponent<PlayerWeaponManager_Inventory>();
+        inventory.onItemChangedCallBack += UpdateUI;
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+    }
+
+    private void UpdateUI()
+    {
+        int i = 0;
+        foreach(AbstractWeaponGeneric weapon in inventory.Weapons)
+        {
+            slots[i].AddItem(weapon);
+            i++;
+        }
+    }
+}
