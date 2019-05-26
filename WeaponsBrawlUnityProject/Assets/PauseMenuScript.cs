ee52132d12fea7e44b04ab7ab518fedcb2bc04c8
mauu95 <mauu95@hotmail.it>
2019-05-26 11:09:01 +0200

    Added PauseMenu

---
+++ WeaponsBrawlUnityProject/Assets/PauseMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour {
+
+    public  GameObject PauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OpenClosePauseMenu();
+    }
+
+    public void OpenClosePauseMenu()
+    {
+        PauseMenuUI.SetActive(!PauseMenuUI.activeSelf);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
