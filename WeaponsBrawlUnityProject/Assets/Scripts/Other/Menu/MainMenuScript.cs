0bdd33380d3226af57fd5e427e85fb7f35f159d6
mauu95 <mauu95@hotmail.it>
2019-05-28 11:47:38 +0200

    REFACTORING added abstract menu

---
+++ WeaponsBrawlUnityProject/Assets/Scripts/Other/Menu/MainMenuScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : AbstractMenu {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
---
+++ WeaponsBrawlUnityProject/Assets/Scripts/Other/Menu/PauseMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : AbstractMenu
+{
+    public GameObject PauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OpenClosePauseMenu();
+    }
+
+    public void OpenClosePauseMenu()
+    {
+        PauseMenuUI.SetActive(!PauseMenuUI.activeSelf);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
+
+
