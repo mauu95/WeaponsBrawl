c8220818b3974c91456cb795a828cc8373e5890b
mauu95 <mauu95@hotmail.it>
2019-06-02 17:49:47 +0200

    Weapon3Punch now deal dmg

--- WeaponsBrawlUnityProject/Assets/Scripts/WeaponList/Weapon3PunchScript.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/WeaponList/Weapon3PunchScript.cs
@@ -6,14 +6,18 @@ public class Weapon3PunchScript : AbstractWeaponGeneric
 {
     public float attackRange;
     public int damagePower;
+    public LayerMask PlayerLayer;
 
     public override void Attack(int charge)
     {
-        RaycastHit2D hitted = Physics2D.Raycast(firePoint.position, firePoint.right, attackRange);
+        RaycastHit2D hitted = Physics2D.Raycast(firePoint.position, firePoint.right, attackRange, PlayerLayer);
+
+        Debug.DrawRay(new Vector3(firePoint.position.x, firePoint.position.y, 0), firePoint.right * attackRange, Color.yellow, 5f, true);
 
         if (hitted)
         {
             print(hitted.transform.name);
+
             PlayerHealth enemy = hitted.transform.GetComponent<PlayerHealth>();
 
             if (enemy)
