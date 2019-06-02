4621d92f271b413d56c151ad49406abcc8f47a07
mauu95 <mauu95@hotmail.it>
2019-06-02 12:35:49 +0200

    Working on weapon3Punch

---
+++ WeaponsBrawlUnityProject/Assets/Scripts/WeaponList/Weapon3PunchScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon3PunchScript : AbstractWeaponGeneric
+{
+    public float attackRange;
+    public int damagePower;
+
+    public override void Attack(int charge)
+    {
+        RaycastHit2D hitted = Physics2D.Raycast(firePoint.position, firePoint.right, attackRange);
+
+        if (hitted)
+        {
+            PlayerHealth enemy = hitted.transform.GetComponent<PlayerHealth>();
+
+            if (enemy)
+                enemy.CmdTakeDamage(damagePower);
+        }
+    }
+}
