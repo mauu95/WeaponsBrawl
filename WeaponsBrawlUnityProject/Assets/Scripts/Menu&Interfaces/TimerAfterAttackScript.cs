117b9b8f09e87fc9b314d3ff46fe42bdec53ad82
mauu95 <mauu95@hotmail.it>
2019-06-08 21:02:02 +0200

    Added timer after attack UI

---
+++ WeaponsBrawlUnityProject/Assets/Scripts/Menu&Interfaces/TimerAfterAttackScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerAfterAttackScript : MonoBehaviour {
+
+    public TextMeshProUGUI timerUI;
+    public float TimeToDisappear = 3f;
+
+    private float timeLeft;
+    
+    public void SetTimer(float sec)
+    {
+        timeLeft = sec;
+        timerUI.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (timerUI.gameObject.activeSelf)
+        {
+            if(timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                timerUI.text = timeLeft.ToString("0");
+            }
+            else
+                StartCoroutine(DisableTimerAfterSec());
+        }
+
+
+    }
+
+    IEnumerator DisableTimerAfterSec()
+    {
+        yield return new WaitForSeconds(TimeToDisappear);
+        timerUI.gameObject.SetActive(false);
+    }
+
+    
+
+}
