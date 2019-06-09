c91606798c2030278a4fa50a1cb8cbd00f897683
mauu95 <mauu95@hotmail.it>
2019-06-09 14:13:39 +0200

    FinalMap Draft

--- WeaponsBrawlUnityProject/Assets/Scripts/Other/MapSizeGizmos.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/Other/MapSizeGizmos.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class MapSizeGizmos : MonoBehaviour {
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.cyan;
-        Vector3 cubeSize = gameObject.transform.localScale * 80;
-        cubeSize.x *= 1.8f;
-        Gizmos.DrawWireCube(transform.position,  cubeSize);
-    }
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.cyan;
+    //    Vector3 cubeSize = gameObject.transform.localScale * 80;
+    //    cubeSize.x *= 1.8f;
+    //    Gizmos.DrawWireCube(transform.position,  cubeSize);
+    //}
 }
