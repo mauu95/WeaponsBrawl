e1f7a8ae0f6d2fa9672a177be442ae4258fed59f
mauu95 <mauu95@hotmail.it>
2019-06-10 14:03:33 +0200

    Added ResourcesUI

---
+++ WeaponsBrawlUnityProject/Assets/ResourceUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourceUI : AbstractInGameInterfaces {
+
+    public TextMeshProUGUI ResourceText;
+
+    public void SetResourceUI(int amount)
+    {
+        ResourceText.text = amount.ToString();
+    }
+}
