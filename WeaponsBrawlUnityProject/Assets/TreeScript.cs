98c9dc58788e2416cce0b3f2b29f92e2452b9db8
mauu95 <mauu95@hotmail.it>
2019-06-12 21:29:58 +0200

    Tree Lights On when near them

---
+++ WeaponsBrawlUnityProject/Assets/TreeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour {
+
+    public Sprite treeOff;
+    public Sprite treeOn;
+
+    public void TurnOnTree(bool yesno)
+    {
+        if (yesno)
+            GetComponent<SpriteRenderer>().sprite = treeOn;
+        else
+            GetComponent<SpriteRenderer>().sprite = treeOff;
+    }
+}
