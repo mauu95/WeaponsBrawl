8920eb6f591690cfb3fa4f7b7c3ebe55f55be764
mauu95 <mauu95@hotmail.it>
2019-06-13 15:44:04 +0200

    Press Q text appear when near tree

--- WeaponsBrawlUnityProject/Assets/TreeScript.cs
+++ WeaponsBrawlUnityProject/Assets/TreeScript.cs
@@ -7,11 +7,21 @@ public class TreeScript : MonoBehaviour {
     public Sprite treeOff;
     public Sprite treeOn;
 
+    public GameObject PressQ;
+
     public void TurnOnTree(bool yesno)
     {
         if (yesno)
+        {
             GetComponent<SpriteRenderer>().sprite = treeOn;
+            PressQ.SetActive(true);
+        }
+
         else
+        {
             GetComponent<SpriteRenderer>().sprite = treeOff;
+            PressQ.SetActive(false);
+        }
+
     }
 }
