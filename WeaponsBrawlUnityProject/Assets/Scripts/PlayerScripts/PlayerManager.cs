2d9d6a02b644e65ac7ad47c3c56d28719734cf75
mauu95 <mauu95@hotmail.it>
2019-06-14 15:00:27 +0200

    Weapons buff

--- WeaponsBrawlUnityProject/Assets/Scripts/PlayerScripts/PlayerManager.cs
+++ WeaponsBrawlUnityProject/Assets/Scripts/PlayerScripts/PlayerManager.cs
@@ -89,7 +89,7 @@ public class PlayerManager : NetworkBehaviour {
 
     private void SetVelocity(float velx, float vely)
     {
-        print("Setting velocity of " + name + "to : velx= " + velx + " vely= " + vely);
+        //print("Setting velocity of " + name + "to : velx= " + velx + " vely= " + vely);
         GetComponent<Rigidbody2D>().velocity = new Vector2(velx, vely);
     }
 
